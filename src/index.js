8fe005cef6f605caac75f79aab15bd51d526d0fb
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-21 14:22:12 +0300

    134. Redux

--- src/index.js
+++ src/index.js
@@ -3,22 +3,54 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux"
 
 
 //STORE => globalized state
 
 //ACTION => arttırma
 
+const increment = () =>{
+  return{
+    type:"increment"
+  }
+};
+
+const decrement = ()=>{
+  return{
+    type:"decrement"
+  }
+}
+
 
 
 //REDUCER => ne dispatch edilirse onu çalıştırır,actiona göre story'i modify eder.
 
+const counter = (state=0,action)=>{
+  switch (action.type) {
+    case "increment":
+      return state +1;
 
+      case "decrement":
+        return state -1;
+  }
+}
+
+let store = createStore(counter);
 
 // Display it on the Screen 
 
+store.subscribe(()=> console.log(store.getState()));
+
 
 //DİSPATCH => action u execute etdigimiz yanı çalıstırdıgımız yerdir.
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+
+
+
+
 
 
 
